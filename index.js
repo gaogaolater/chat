46cc4534fa5730de9bb2484217c3ee6d9f49e224
James <362115359@qq.com>
2016-08-18 23:56:37 +0800

    完成第一版

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use('/public',express.static(__dirname+'/public'));
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
@@ -30,25 +32,42 @@ io.on('connection',function(socket){
 
     //断开连接
     socket.on('disconnect', function(){
+        var disconnGuy = socketMap[socket.id];
+        var other = disconnGuy.other;
+        if(other){
+            //已配对，通知对方并将对方放入等待区
+            other.socket.emit("drop",1);
+            other.other = null;
+            if(other.sex==1){
+                waitingRom.boy.push(other);
+            }
+            else{
+                waitingRom.girl.push(other);
+            }
+        }
+        else{
+            //如果还没配对，在等待区将其删除
+            var deleted = false;
+            if(waitingRom.boy.length>0){
+                for(var i=0;i<waitingRom.boy.length;i++){
+                    if(waitingRom.boy[i].socket.id == socket.id){
+                        waitingRom.boy.splice(i,1);
+                        deleted=true;
+                        break;
+                    }
+                }
+            }
+            if(waitingRom.girl.length>0 && deleted==false){
+                for(var i=0;i<waitingRom.girl.length;i++){
+                    if(waitingRom.girl[i].socket.id == socket.id){
+                        waitingRom.girl.splice(i,1);
+                        break;
+                    }
+                }
+            }
+        }
+        
         delete socketMap[socket.id];
-        var deleted = false;
-        if(waitingRom.boy.length>0){
-            for(var i=0;i<waitingRom.boy.length;i++){
-                if(waitingRom.boy[i].socket.id == socket.id){
-                    waitingRom.boy.splice(i,1);
-                    deleted=true;
-                    break;
-                }
-            }
-        }
-        if(waitingRom.girl.length>0 && deleted==false){
-            for(var i=0;i<waitingRom.girl.length;i++){
-                if(waitingRom.girl[i].socket.id == socket.id){
-                    waitingRom.girl.splice(i,1);
-                    break;
-                }
-            }
-        }
         console.log('disconnect '+socket.id);
     });
 
@@ -57,8 +76,8 @@ io.on('connection',function(socket){
         var obj = socketMap[socket.id];
         var other = obj.other;
         if(other){
-            obj.socket.emit('chat',{ok:1,type:1,msg:msg});
-            other.socket.emit('chat',{ok:1,type:1,msg:msg});
+            obj.socket.emit('chat',{ok:1,type:1,you:true,sex:obj.sex,msg:msg});
+            other.socket.emit('chat',{ok:1,type:1,you:false,sex:obj.sex,msg:msg});
         }
         else{
             obj.socket.emit('chat',{ok:0,type:1,error:'no body'});
@@ -85,7 +104,7 @@ setInterval(function(){
     console.log('boy:'+waitingRom.boy.length + ' girl:'+waitingRom.girl.length);
     if(waitingRom.boy.length>0 && waitingRom.girl.length>0){
         var min = waitingRom.boy.length;
-        if(waitingRom.girl.length<waitingRom.boy.length){
+        if(waitingRom.girl.length < waitingRom.boy.length){
             min = waitingRom.girl.length;
         }
         for(var i=waitingRom.girl.length-1;i>=0;i--){
@@ -93,8 +112,11 @@ setInterval(function(){
             var boy = waitingRom.boy[i];
             girl.other = boy;
             boy.other = girl;
-            waitingRom.girl.pop();
-            waitingRom.boy.pop();
+            //提醒已经配对
+            boy.socket.emit("waiting",0);
+            girl.socket.emit("waiting",0);
+            waitingRom.girl.splice(i,1);
+            waitingRom.boy.splice(i,1);
         }
     }
     if(waitingRom.boy.length>0){
@@ -102,7 +124,7 @@ setInterval(function(){
             waitingRom.boy[i].socket.emit('waiting',1);
         }
     }
-    if(waitingRom.girl.length>0 && deleted==false){
+    if(waitingRom.girl.length>0){
         for(var i=0;i<waitingRom.girl.length;i++){
              waitingRom.girl[i].socket.emit('waiting',1);
         }
