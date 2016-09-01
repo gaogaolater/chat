8a463874006cd5fb001db1e727d1981e338f050a
James <362115359@qq.com>
2016-09-02 00:39:50 +0800

    添加了测试代码

--- router/gameio.js
+++ router/gameio.js
@@ -1,5 +1,18 @@
 module.exports = function(io,socket){
+    var uuid = require("node-uuid");
+    var room = {};
+    var roomCount = 0;
+
     console.log('a user connect');
+
+    socket.on("createRoom",function(msg){
+        io.emit("move",msg);
+    });
+
+    socket.on("createRoom",function(msg){
+        io.emit("move",msg);
+    });
+
     socket.on("move",function(msg){
         io.emit("move",msg);
     });
