e048d5b0421d25c9dcc45d100d4822ed5a6afa13
James <362115359@qq.com>
2016-09-28 09:17:17 +0800

    修改了startPlay

--- router/chat.js
+++ router/chat.js
@@ -132,7 +132,7 @@ chatContext.ioinit = function (io) {
             var obj = socketMap[socket.id];
             obj.name = info.name;
             obj.sex = info.sex;
-            if (obj.sex == 1) {
+            if (obj.sex == 1) { 
                 waitingRom.boy.push(obj);
             }
             else {
@@ -145,34 +145,34 @@ chatContext.ioinit = function (io) {
 }
 
 setInterval(function () {
-        //console.log('boy:'+waitingRom.boy.length + ' girl:'+waitingRom.girl.length);
-        if (waitingRom.boy.length > 0 && waitingRom.girl.length > 0) {
-            var min = waitingRom.boy.length;
-            if (waitingRom.girl.length < waitingRom.boy.length) {
-                min = waitingRom.girl.length;
-            }
-            for (var i = waitingRom.girl.length - 1; i >= 0; i--) {
-                var girl = waitingRom.girl[i];
-                var boy = waitingRom.boy[i];
-                girl.other = boy;
-                boy.other = girl;
-                //提醒已经配对
-                boy.socket.emit("waiting", 0);
-                girl.socket.emit("waiting", 0);
-                boy.socket.emit('chat', { ok: 1, type: 1, you: false, sex: 0, msg: '你好啊' });
-                girl.socket.emit('chat', { ok: 1, type: 1, you: false, sex: 1, msg: '你好啊' });
-                waitingRom.girl.splice(i, 1);
-                waitingRom.boy.splice(i, 1);
-            }
+    //console.log('boy:'+waitingRom.boy.length + ' girl:'+waitingRom.girl.length);
+    if (waitingRom.boy.length > 0 && waitingRom.girl.length > 0) {
+        var min = waitingRom.boy.length;
+        if (waitingRom.girl.length < waitingRom.boy.length) {
+            min = waitingRom.girl.length;
         }
-        if (waitingRom.boy.length > 0) {
-            for (var i = 0; i < waitingRom.boy.length; i++) {
-                waitingRom.boy[i].socket.emit('waiting', 1);
-            }
+        for (var i = waitingRom.girl.length - 1; i >= 0; i--) {
+            var girl = waitingRom.girl[i];
+            var boy = waitingRom.boy[i];
+            girl.other = boy;
+            boy.other = girl;
+            //提醒已经配对
+            boy.socket.emit("waiting", 0);
+            girl.socket.emit("waiting", 0);
+            boy.socket.emit('chat', { ok: 1, type: 1, you: false, sex: 0, msg: '你好啊' });
+            girl.socket.emit('chat', { ok: 1, type: 1, you: false, sex: 1, msg: '你好啊' });
+            waitingRom.girl.splice(i, 1);
+            waitingRom.boy.splice(i, 1);
         }
-        if (waitingRom.girl.length > 0) {
-            for (var i = 0; i < waitingRom.girl.length; i++) {
-                waitingRom.girl[i].socket.emit('waiting', 1);
-            }
+    }
+    if (waitingRom.boy.length > 0) {
+        for (var i = 0; i < waitingRom.boy.length; i++) {
+            waitingRom.boy[i].socket.emit('waiting', 1);
         }
-    }, 1000);
+    }
+    if (waitingRom.girl.length > 0) {
+        for (var i = 0; i < waitingRom.girl.length; i++) {
+            waitingRom.girl[i].socket.emit('waiting', 1);
+        }
+    }
+}, 1000);
